76704af0e34ffcb661a628f8176ef9ab48d0801c
Geez-Us <noahsullivan382@gmail.com>
2022-12-25 10:23:57 -0800

    script relocation

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var pass8index = [1, 2, 3, 4, 5, 6, 7, 8];
+
 
 function createpassword () {
+    var pass8index = [1, 2, 3, 4, 5, 6, 7, 8];
+    var uplttrs = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"];
+    var generate = Math.floor(Math.random() * uplttrs.length);
+    var UCchoice = uplttrs[generate];
+    var passgen1 = Math.floor(Math.random() * pass8index.length);
+    console.log(pass8index);
 
     var prompt = window.confirm("Would You Like A New Password?");
 
@@ -13,13 +19,8 @@ function createpassword () {
     if (!upcsltrs) {
         window.alert("No big deal! onto the next question!");
     }else{
-        var uplttrs = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"];
-        var generate = Math.floor(math.random() * uplttrs.length);
-        var UCchoice = uplttrs[generate];
-        var passgen1 = Math.floor(math.random() * pass8index.length);
+
         var pass1Choice = pass8index[passgen1];
-    
-        console.log(pass8index.values);
 
     };
     var lwcsltrs = window.confirm("Would you like lowercase characters in your password?");
