703572a9c6d7221ffd886fd429bbdadd8b36d52a
Geez-Us <noahsullivan382@gmail.com>
2022-12-28 12:22:22 -0800

    debug Y/SC Y/LC Y/UC NMC N/A;N/UC Y/SC Y/LC NMC N/A

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ console.log(createint(1));
 function createpassword() {
     var prompt = window.confirm("Would You Like A New Password?");
 
-    var countcharacter = window.prompt("How many characters would you like?");
-    function initresponse () {    
+    
+    function initresponse () { 
+        var countcharacter = window.prompt("How many characters would you like?");   
         if (countcharacter == "8") {
             alert("Pick a number that is greater than 8");
             return; }
         else if (countcharacter == "9"){
            alert("Perfect, this is a manageable value");
         }
-        else if (countcharacter > 9){
+        else if (countcharacter > 9 && countcharacter < 128){
             alert("Perfect, this is a manageable value");
             function additemstopass() {
 
@@ -30,8 +31,8 @@ function createpassword() {
             console.log(password);
         }
         else {
-            alert("please input a character 'Ex: 10': \n in numeric form above 8 \n and less than 128");
-            return;
+            if (confirm("please input a character 'Ex: 10': \n in numeric form above 8 \n and less than 128") == true);
+            initresponse();
         };
         
         };
@@ -221,12 +222,6 @@ function createpassword() {
                 for (var i = 0; i < passlength; i++) {
                     randomassign(i);
                 };
-            
-                function repeat () {
-                    if (password[0] === 0) {
-                        randomassign(password[0]);
-                    }
-                }
                 console.log(password[createint(9)]);
             
                 console.log(password);
@@ -235,6 +230,47 @@ function createpassword() {
             
                 };
             }else{
+                function rep1LC(lcvalue) {
+                    var lclttrs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+                    var lcgenerate = Math.floor(Math.random() * lclttrs.length);
+                    password[lcvalue] = lclttrs[lcgenerate];
+                    console.log(password);
+                    };
+
+                function rep1SC(scvalue) {
+                    var spcllttrs = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "[", "]", "{", "}", "|", ";", ":", "'", "/", "?", "<",">",".",","];
+                    var spclgenerate = Math.floor(Math.random() * spcllttrs.length);
+                    password[scvalue] = spcllttrs[spclgenerate];
+                    console.log(password);
+                    };
+
+                var numchts = window.confirm("Would you like numbers in your password?");
+                if (!numchts) {
+                    function randomassign(a) {
+                        var dice = createint(2);
+                        if (dice === 0) {
+                            rep1LC(a);
+                        }
+                        else if (dice === 1) {
+                            rep1SC(a);
+                        };
+                        
+                        for (var i = 0; i < passlength; i++) {
+                            randomassign(i);
+                        };
+                
+                    };
+                }else{
+
+
+
+                function rep1NMC(nmcvalue) {
+                    var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+                    var nmbrgenerate = Math.floor(Math.random() * numbers.length);
+                    password[nmcvalue] = numbers[nmbrgenerate];
+                    console.log(password);
+                    };
+
                 function rep1SC(scvalue) {
                     var spcllttrs = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "[", "]", "{", "}", "|", ";", ":", "'", "/", "?", "<",">",".",","];
                     var spclgenerate = Math.floor(Math.random() * spcllttrs.length);
@@ -243,16 +279,25 @@ function createpassword() {
                     };
 
                     function randomassign(a) {
-                        var dice = createint(2);
+                        var dice = createint(3);
                         if (dice === 0) {
                             rep1LC(a);
                         }
                         else if (dice === 1) {
                             rep1SC(a);
-                        }
+                        } 
+                        else if (dice === 2) {
+                            rep1NMC(a);
+                        };
                     };
                 for (var i = 0; i < passlength; i++) {
                     randomassign(i);
+                    };
+
+                console.log(password[createint(passlength)]);
+                
+                console.log(password);       
+                window.alert("Generating Password Now, click when ready...")
                 };
             };
         };
@@ -442,70 +487,117 @@ function createpassword() {
                 };
             }else{
 
-            function rep1UC(ucvalue) {
-                var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-                var alphkey = Math.floor(Math.random() * uppercase.length);
-                password[ucvalue] = uppercase[alphkey];
-                console.log(password);
-                };
+                var numchts = window.confirm("Would you like numbers in your password?");
+            
+                if (!numchts) {
+                    function rep1UC(ucvalue) {
+                        var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+                        var alphkey = Math.floor(Math.random() * uppercase.length);
+                        password[ucvalue] = uppercase[alphkey];
+                        console.log(password);
+                        };
 
 
-            function rep1LC(lcvalue) {
-                var lclttrs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
-                var lcgenerate = Math.floor(Math.random() * lclttrs.length);
-                password[lcvalue] = lclttrs[lcgenerate];
-                console.log(password);
-                };
+                    function rep1LC(lcvalue) {
+                        var lclttrs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+                        var lcgenerate = Math.floor(Math.random() * lclttrs.length);
+                        password[lcvalue] = lclttrs[lcgenerate];
+                        console.log(password);
+                        };
+                        
+                    function rep1SC(scvalue) {
+                        var spcllttrs = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "[", "]", "{", "}", "|", ";", ":", "'", "/", "?", "<",">",".",","];
+                        var spclgenerate = Math.floor(Math.random() * spcllttrs.length);
+                        password[scvalue] = spcllttrs[spclgenerate];
+                        console.log(password);
+                        };
                 
-            function rep1SC(scvalue) {
-                var spcllttrs = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "[", "]", "{", "}", "|", ";", ":", "'", "/", "?", "<",">",".",","];
-                var spclgenerate = Math.floor(Math.random() * spcllttrs.length);
-                password[scvalue] = spcllttrs[spclgenerate];
-                console.log(password);
-                };
+                    window.alert("Generating Password Now, click when ready...");
+
+                    function randomassign(a) {
+                        var dice = createint(3);
+                        if (dice === 0) {
+                            rep1UC(a);
+                        } 
+                        else if (dice === 1) {
+                            rep1LC(a);
+                        }
+                        else if (dice === 2) {
+                            rep1SC(a);
+                        };
+                        };
+                    for (var i = 0; i < passlength; i++) {
+                        randomassign(i);
+                    };
                 
-            function rep1NMC(nmcvalue) {
-                var numbers = ["1","2","3","4","5","6","7","8","9","0"];
-                var nmbrgenerate = Math.floor(Math.random() * numbers.length);
-                password[nmcvalue] = numbers[nmbrgenerate];
-                console.log(password);
-                };
+                }else{
+                    window.alert("Generating Password Now, click when ready...");
+
+                    function rep1UC(ucvalue) {
+                        var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+                        var alphkey = Math.floor(Math.random() * uppercase.length);
+                        password[ucvalue] = uppercase[alphkey];
+                        console.log(password);
+                        };
+
+
+                    function rep1LC(lcvalue) {
+                        var lclttrs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+                        var lcgenerate = Math.floor(Math.random() * lclttrs.length);
+                        password[lcvalue] = lclttrs[lcgenerate];
+                        console.log(password);
+                        };
+                        
+                    function rep1SC(scvalue) {
+                        var spcllttrs = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "[", "]", "{", "}", "|", ";", ":", "'", "/", "?", "<",">",".",","];
+                        var spclgenerate = Math.floor(Math.random() * spcllttrs.length);
+                        password[scvalue] = spcllttrs[spclgenerate];
+                        console.log(password);
+                        };
+                        
+                    function rep1NMC(nmcvalue) {
+                        var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+                        var nmbrgenerate = Math.floor(Math.random() * numbers.length);
+                        password[nmcvalue] = numbers[nmbrgenerate];
+                        console.log(password);
+                        };
 
 
 
-                // assign function for each character possibility, and then assign each index a character based on integer outcome 0,1,2,3 
+                        // assign function for each character possibility, and then assign each index a character based on integer outcome 0,1,2,3 
 
-            function randomassign(a) {
-                var dice = createint(4);
-                if (dice === 0) {
-                    rep1UC(a);
-                } 
-                else if (dice === 1) {
-                    rep1LC(a);
-                }
-                else if (dice === 2) {
-                    rep1SC(a);
-                }
-                else if (dice === 3) {
-                    rep1NMC(a);
-                };
-                };
-            for (var i = 0; i < passlength; i++) {
-                    randomassign(i);
-                };
+                    function randomassign(a) {
+                        var dice = createint(4);
+                        if (dice === 0) {
+                            rep1UC(a);
+                        } 
+                        else if (dice === 1) {
+                            rep1LC(a);
+                        }
+                        else if (dice === 2) {
+                            rep1SC(a);
+                        }
+                        else if (dice === 3) {
+                            rep1NMC(a);
+                        };
+                        };
+                    for (var i = 0; i < passlength; i++) {
+                            randomassign(i);
+                        };
 
-                function repeat () {
-                    if (password[0] === 0) {
-                        randomassign(password[0]);
-                    }
-                }
-                console.log(password[createint(9)]);
+                        function repeat () {
+                            if (password[0] === 0) {
+                                randomassign(password[0]);
+                            }
+                        }
+                        console.log(password[createint(9)]);
 
-                console.log(password);
-                };
+                        console.log(password);
                 };
             };
-password.join();
+        };
+password.join("");
+    };
 };
 
 
